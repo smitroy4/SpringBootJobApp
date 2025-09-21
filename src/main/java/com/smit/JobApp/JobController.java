b1a6ea7f3c4e1ff8e4110d7eabb98cc5ac7b27b1
smitroy4 <smitroy22@gmail.com>
2025-09-21 23:58:33 +0530

    Test Commit

---
+++ src/main/java/com/smit/JobApp/JobController.java
@@ -0,0 +1,12 @@
+package com.smit.JobApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+    @RequestMapping({"/","home"})
+    public String home(){
+        return "home";
+    }
+}
---
+++ src/test/java/com/smit/JobApp/SpringBootJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smit.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
